623b01daf9e0369d6f2b79db8e4211f17325d107
Brunopjunq <bruno_junq@poli.ufrj.br>
2023-02-28 15:31:47 -0300

    fix: add weight_goal on route PUT /user

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,12 +1,13 @@
 import prisma from "../database/database.js";
 
-async function updateUser(caloriesGoal: number,name: string, userId: number) {
+async function updateUser(caloriesGoal: number, weight_goal: number, name: string, userId: number) {
     return prisma.users.update({
         where: {
             id: userId,
         }, data: {
             calories_goal: caloriesGoal,
             name: name,
+            weight_goal: weight_goal,
         }
     })
 };
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,7 +1,7 @@
 import userRepository from "../repositories/user-repository.js";
 
-async function updateUser(caloriesGoal: number,name: string, userId: number) {
-    const updatedUser = await userRepository.updateUser(caloriesGoal,name,userId);
+async function updateUser(caloriesGoal: number, weight_goal: number,name: string, userId: number) {
+    const updatedUser = await userRepository.updateUser(caloriesGoal,weight_goal,name,userId);
     return updatedUser;
 };
 
