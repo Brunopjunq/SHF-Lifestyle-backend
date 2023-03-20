32ce7361c5914add75a470ea43f1891bf24205da
Brunopjunq <bruno_junq@poli.ufrj.br>
2023-03-20 14:30:12 -0300

    fix: change route /user

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -2,6 +2,7 @@ import { AuthenticatedRequest } from "../middlewares/validate-token.js";
 import { Response } from "express";
 import userService from "../services/user-service.js";
 import httpStatus from "http-status";
+import { exclude } from "../utils/prisma-utils.js";
 
 export async function updateUser(req: AuthenticatedRequest, res: Response) {
     try {
@@ -9,7 +10,7 @@ export async function updateUser(req: AuthenticatedRequest, res: Response) {
         const { water_goal, calories_goal, weight_goal, name } = req.body;
     
         const updatedUser = await userService.updateUser(water_goal,calories_goal,weight_goal,name, userId);
-        return res.status(httpStatus.OK).send(updatedUser);
+        return res.status(httpStatus.OK).send(exclude(updatedUser, 'password'));
     } catch (error) {
         return res.sendStatus(httpStatus.BAD_REQUEST); 
     }
